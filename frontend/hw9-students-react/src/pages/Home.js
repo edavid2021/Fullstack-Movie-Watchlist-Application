66c2c3477788e468c6eb35ae9763aa699d8ae7eb
giacobn <giacobn@comcast.net>
2023-04-21 16:05:45 -0400

    card for displaying movies

--- frontend/hw9-students-react/src/pages/Home.js
+++ frontend/hw9-students-react/src/pages/Home.js
@@ -1,8 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 export default function Home(){
     return(
-        <>
-            <h1>Home</h1>
-        </>
+        <Card style={{ width: '12rem' }}>
+  <Card.Img
+    variant="top"
+    src="joker.jpeg"
+    style={{ width: '120px', height: '150px', objectFit: 'cover' }}
+    className="mx-auto d-block"
+  />
+  <Card.Body className="text-center">
+    <Card.Title style={{ fontSize: '1rem', lineHeight: '1.2' }}>Card Title</Card.Title>
+    <Card.Text style={{ fontSize: '0.8rem', lineHeight: '1.2' }}>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+      
+
     )
 }
