80a7b89115e92a4bff40b523b690e646e07484a2
giacobn <giacobn@comcast.net>
2023-04-26 19:21:06 -0400

    working

--- frontend/hw9-students-react/src/pages/MovieDetails.js
+++ frontend/hw9-students-react/src/pages/MovieDetails.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Image, Badge } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import axios from 'axios';
 
 const MovieDetails = () => {
   const { movieID } = useParams();
   const [movie, setMovie] = useState(null);
+  const [directors, setDirectors] = useState([]);
+  const [writers, setWriters] = useState([]);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -16,6 +18,14 @@ const MovieDetails = () => {
           }
         });
         setMovie(response.data);
+
+        const creditsResponse = await axios.get(`https://api.themoviedb.org/3/movie/${movieID}/credits`, {
+          params: {
+            api_key: '8634de54298be041f009b7c918664433'
+          }
+        });
+        setDirectors(creditsResponse.data.crew.filter((crew) => crew.job === 'Director'));
+        setWriters(creditsResponse.data.crew.filter((crew) => crew.department === 'Writing'));
       } catch (error) {
         console.error(error);
       }
@@ -28,42 +38,30 @@ const MovieDetails = () => {
     return <div>Loading...</div>;
   }
 
+  const genres = movie.genres.map((genre) => genre.name).join(', ');
+
   return (
-    <Container fluid>
+    <Container>
       <Row>
         <Col md={4}>
-          <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} fluid/>
+          <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} fluid />
         </Col>
         <Col md={8}>
           <div className="movie-details">
             <h1>{movie.title}</h1>
-            <p>
-              <Badge pill variant="secondary">{movie.release_date.split('-')[0]}</Badge>
-              &nbsp;
-              {movie.genres.map((genre, index) => (
-                <Badge key={index} pill variant="secondary">{genre.name}</Badge>
-              ))}
-              &nbsp;&nbsp;
-              {movie.production_countries.map((country, index) => (
-                <span key={index}>{country.iso_3166_1}</span>
-              ))}
-              &nbsp;&nbsp;
-              <Badge pill variant="secondary">{movie.runtime} min</Badge>
-            </p>
+            <div className="genres">{genres}</div>
             <p>{movie.overview}</p>
-            <p>
-              <strong>Director:</strong> {movie.credits.crew.find((crew) => crew.job === 'Director').name}
-            </p>
-            <p>
-              <strong>Stars:</strong> {movie.credits.cast.slice(0, 3).map((cast, index) => (
-                <span key={index}>{cast.name}&nbsp;</span>
-              ))}
-            </p>
-            <p>
-              <strong>Rating:</strong> {movie.vote_average}/10
-              &nbsp;&nbsp;
-              <span className="text-muted">{movie.vote_count} votes</span>
-            </p>
+            <div className="row">
+              <div className="col-sm-6">
+                <p>Release date: {movie.release_date}</p>
+                <p>Runtime: {movie.runtime} minutes</p>
+                <p>Rating: {movie.vote_average}/10 ({movie.vote_count} votes)</p>
+              </div>
+              <div className="col-sm-6">
+                <p>Directors: {directors.map((director) => director.name).join(', ')}</p>
+                <p>Writers: {writers.map((writer) => writer.name).join(', ')}</p>
+              </div>
+            </div>
           </div>
         </Col>
       </Row>
