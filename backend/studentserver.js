117a6e4ee8e472ac4e3fc5d28e912874738f6ba7
Emmanuel David <edavid2021@fau.edu>
2023-04-28 20:52:39 -0400

    Latest Page and Watchlist Button

--- backend/studentserver.js
+++ backend/studentserver.js
@@ -20,50 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// GET requests to render HTML pages
-/*
-app.get('/',function(req,res){
-  res.render('home', { pageTitle: 'Home' });
-});
-app.get('/listStudent',function(req,res){
-  res.render('listStudent', { pageTitle: 'Students' });
-});
-app.get('/addStudent',function(req,res){
-  res.render('addStudent', { pageTitle: 'Add a Student' });
-});
-app.get('/displayStudent',function(req,res){
-  res.render('displayStudent', { pageTitle: 'Single Student Record' });
-});
-app.get('/deleteStudent',function(req,res){
-  res.render('deleteStudent', { pageTitle: '' });
-});
-app.get('/updateStudent',function(req,res){
-  res.render('updateStudent', { pageTitle: 'Edit Student' });
-});*/
-// POST request to add a new student record to MongoDB
-app.post('/students', async function(req, res) {
-  var record_id = new Date().getTime();
-  // Creating a new object to store the student record
-  var obj = {};
-  obj.record_id = record_id;
-  obj.first_name = req.body.first_name;
-  obj.last_name = req.body.last_name;
-  obj.gpa = req.body.gpa;
-  obj.enrolled = req.body.enrolled;
-  const client = new MongoClient(uri,{useUnifiedTopology: true});
-  try {
-   
-    const dbo = client.db(config.db.name);
-    await dbo.collection(config.db.collection).insertOne(obj);
-    console.log("Student record added successfully");
-    client.close();
-    res.send('Student record added successfully');
-  } catch(err) {
-    console.log(err);
-    res.status(500).send('Error adding student record');
-  }
-});
 //route for getting data from the moviedb api, this is for the trending/home page
 
 app.get('/trending', async (req, res) => {
@@ -86,144 +42,42 @@ app.get('/trending', async (req, res) => {
   }
 });
 
-
-// Define a route that listens for requests to retrieve a specific student record by its record_id
-app.get('/students/:record_id', async function(req, res) {
-  // Retrieve the record_id from the request parameters
-  var record_id = req.params.record_id;
+app.get('/latest', async (req, res) => {
+  const { page } = req.query;
 
   try {
-    // Connect to the MongoDB database
-    const client = await MongoClient.connect(uri);
-    const db = client.db(config.db.name);
-    // Select the 'Students' collection
-    const collection = db.collection('Students');
-
-    // Find the student record with the given record_id
-    const result = await collection.findOne({record_id: parseInt(record_id)});
-    if (!result) { // If no record was found, send a 404 error response
-      var rsp_obj = {};
-      rsp_obj.record_id = record_id;
-      rsp_obj.message = 'error - resource not found';
-      return res.status(404).send(rsp_obj);
-    } else { // If a record was found, send it as a JSON response
-      return res.status(200).send(result);
-    }
-  } catch(err) { // If an error occurs, log it and send a 500 error response
-    console.log('Error:', err);
-    return res.status(500).send({"message": "Error finding student record"});
+    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/now_playing`, {
+      params: {
+        api_key: '8634de54298be041f009b7c918664433',
+        language: 'en-US',
+        page: page
+      }
+    });
+    res.status(200).send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
   }
 });
 
-
-// Handle GET requests to the "/students" endpoint
-app.get('/students', async function(req, res) {
-
-  // Connect to MongoDB
-  try {
-    const client = await MongoClient.connect(uri,{ useUnifiedTopology: true });
-    const dbo = client.db(config.db.name);
-
-    // Get the search query parameter (if it exists)
-    const searchQuery = req.query.lastName;
-
-    // Define the query object based on the search query parameter
-    const query = searchQuery ? {last_name: searchQuery} : {};
-
-    // Query the "Students" collection for documents matching the query object
-    const result = await dbo.collection(config.db.collection).find(query).toArray();
-    const obj = { students: result };
-
-    // If no documents were found, add a message to the response object
-    if (result.length === 0) {
-      obj.message = "No data found";
-    }
-
-    // Send the response object with a 200 OK status code
-    res.status(200).send(obj);
-
-    // Close the database connection
-    client.close();
-
-  } catch(err) {
-    // Log any errors and send a 500 Internal Server Error response
-    console.log(err);
-    res.status(500).send({"message": "error - internal server error"});
-  }
-});
-
-
-
-// Define an endpoint for updating a student record using a PUT request
-app.put('/students/:record_id', async function(req, res) {
-  // Extract the record ID from the request parameters
-  const record_id = req.params.record_id;
-  // Extract the updated record information from the request body
-  const { first_name, last_name, gpa, enrolled } = req.body;
+app.get('/search', async (req, res) => {
+  const { query, page } = req.query;
 
   try {
-    // Connect to the database
-    const client = await MongoClient.connect(uri);
-    const db = client.db(config.db.name);
-    const collection = db.collection('Students');
-
-    // Update the record with the given ID with the new information
-    const result = await collection.updateOne(
-      { record_id: parseInt(record_id) },
-      { $set: { first_name, last_name, gpa, enrolled } }
-    );
-
-    // If the update did not modify any records, the record with the given ID was not found
-    if (result.modifiedCount === 0) {
-      const rsp_obj = { record_id, message: 'error - resource not found' };
-      return res.status(404).send(rsp_obj);
-    }
-
-    // If the update was successful, return a success message
-    const rsp_obj = { record_id, message: 'successfully updated' };
-    return res.status(201).send(rsp_obj);
-
-  } catch (err) {
-    // If an error occurred, log the error and return an error message
-    console.log('Error updating student record:', err);
-    return res.status(500).send({ message: 'Error updating student record' });
+    const { data } = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
+      params: {
+        api_key: '8634de54298be041f009b7c918664433',
+        language: 'en-US',
+        query: query,
+        page: page
+      }
+    });
+    res.status(200).send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
   }
 });
-
-
-
-
- //end put method
-
-// DELETE request handler for '/students/:record_id'
-app.delete('/students/:record_id', async function(req, res) {
-  var record_id = req.params.record_id;
-
-  try {
-    // connect to the database
-    const client = await MongoClient.connect(uri);
-    const db = client.db(config.db.name);
-    const collection = db.collection('Students');
-
-    // delete the record with the specified record_id
-    const result = await collection.deleteOne({record_id: parseInt(record_id)});
-
-    // check if any record was deleted and send appropriate response
-    if (result.deletedCount === 0) {
-      var rsp_obj = {};
-      rsp_obj.record_id = record_id;
-      rsp_obj.message = 'error - resource not found';
-      return res.status(404).send(rsp_obj);
-    } else {
-      return res.status(200).send({"message": "Student record deleted successfully"});
-    }
-  } catch (err) {
-    // handle errors
-    console.log('Error deleting student record:', err);
-    return res.status(500).send({"message": "Error deleting student record"});
-  }
-});
- //end delete method
 
 app.listen(5678); //start the server
 console.log('Server is running...');
--- frontend/hw9-students-react/src/pages/Latest.js
+++ frontend/hw9-students-react/src/pages/Latest.js
@@ -1,113 +1,204 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import {useNavigate} from 'react-router-dom';
 
-function Latest() {
-  
-  return (
-    <>
-      <section className="section p-3 bg-light border-bottom border-2 border-dark" >
+
+
+export default function Latest() {
+    const [movies, setMovies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [active, setActive] = useState(false);
+    var [Watchlist, setWatchlist] = useState([]);
+
+
+    useEffect(() => {
+        async function fetchMovies(page) {
+            const response = await axios.get(`http://localhost:5678/latest?page=${page}`);
+            const { data } = response;
+            setMovies(data.results);
+            setTotalPages(data.total_pages);
+        }
+        fetchMovies(currentPage);
+    }, [currentPage]);
+
+    function handlePageChange(page) {
+        setCurrentPage(page);
+    }
+
+    function renderPagination() {
+        const PAGE_RANGE_DISPLAYED = 5;
+        const MARGIN_PAGES_DISPLAYED = 1;
+        let pagination = [];
+
+        if (totalPages <= PAGE_RANGE_DISPLAYED) {
+            for (let i = 1; i <= totalPages; i++) {
+                pagination.push(
+                    <button
+                        key={i}
+                        className={`btn btn-outline-dark me-2 ${i === currentPage ? 'active' : ''}`}
+                        onClick={() => handlePageChange(i)}
+                    >
+                        {i}
+                    </button>
+                );
+            }
+        } else {
+            let leftSide = currentPage - Math.floor(PAGE_RANGE_DISPLAYED / 2);
+            let rightSide = currentPage + Math.floor(PAGE_RANGE_DISPLAYED / 2);
+
+            if (leftSide < MARGIN_PAGES_DISPLAYED + 1) {
+                leftSide = MARGIN_PAGES_DISPLAYED + 1;
+                rightSide = PAGE_RANGE_DISPLAYED;
+            }
+
+            if (rightSide > totalPages - MARGIN_PAGES_DISPLAYED) {
+                leftSide = totalPages - PAGE_RANGE_DISPLAYED + 1;
+                rightSide = totalPages - MARGIN_PAGES_DISPLAYED;
+            }
+
+            pagination.push(
+                <button
+                    key={1}
+                    className={`btn btn-outline-dark me-2 ${1 === currentPage ? 'active' : ''}`}
+                    onClick={() => handlePageChange(1)}
+                >
+                    {1}
+                </button>
+            );
+
+            if (leftSide > MARGIN_PAGES_DISPLAYED + 1) {
+                pagination.push(
+                    <span key="leftEllipsis" className="me-2">
+                        ...
+                    </span>
+                );
+            }
+
+            for (let i = leftSide; i <= rightSide; i++) {
+                pagination.push(
+                    <button
+                        key={i}
+                        className={`btn btn-outline-dark me-2 ${i === currentPage ? 'active' : ''}`}
+                        onClick={() => handlePageChange(i)}
+                    >
+                        {i}
+                    </button>
+                );
+            }
+
+            if (rightSide < totalPages - MARGIN_PAGES_DISPLAYED) {
+                pagination.push(
+                    <span key="rightEllipsis" className="me-2">
+                        ...
+                    </span>
+                );
+            }
+
+            pagination.push(
+                <button
+                    key={totalPages}
+                    className={`btn btn-outline-dark me-2 ${totalPages === currentPage ? 'active' : ''}`}
+                    onClick={() => handlePageChange(totalPages)}
+                >
+                    {totalPages}
+                </button>
+            );
+        }
+
+        return <div>{pagination}</div>;
+    }
+
+    function addWatchlist(event) {
+        event.preventDefault();
+        var id = parseInt(event.target.id);
+        setWatchlist([...Watchlist, id]);
+        console.log(Watchlist);
+    }
+
+    function deleteWatchlist(event) {
+        event.preventDefault();
+        var id = parseInt(event.target.id);
+        setWatchlist(Watchlist.filter(item => item !== id));
+        console.log(Watchlist);
+    }
+
+    return (
+        <>
+            <section className="section p-3 bg-light border-bottom border-2 border-dark">
                 <div className="container d-flex justify-content-between align-items-center">
                     <h3 className="text-center">Latest</h3>
-                
-                <div className="justify-content-md-end">
-                    <button className="btn btn-outline-dark me-2">1</button>
-                    <button className="btn btn-outline-dark me-2">2</button>
-                    <button className="btn btn-outline-dark">3</button>
+
+                    <div className="justify-content-md-end">
+                        {renderPagination()}
+                    </div>
                 </div>
-                </div> 
             </section>
             <section className="section p-3 bg-light">
-                <div class="container">
-                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                        </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 pt-4">
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                        </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+                        {movies.map((movie) => (
+                            <div className="col d-flex" key={movie.id}>
+                                <Link to={`/MovieDetails/${movie.id}`} className="movie-link">
+                                    <div className="card h-100">
+                                        <img
+                                            className="card-img-top"
+                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                            alt={movie.title}
+                                        />
+                                        <div className="card-body d-flex flex-column">
+                                            <h5 className="card-title">{movie.title}</h5>
+                                            <p className="card-text">{movie.overview.substring(0, 100)}...</p>
+
+                                            <div class="d-flex justify-content-between align-items-center mt-auto">
+                                                <p class="card-text">
+                                                    <small class="text-muted">
+                                                        {movie.release_date}
+                                                    </small>
+                                                </p>
+                                                {Watchlist.includes(movie.id) ?
+                                                    <button
+                                                        id={movie.id}
+                                                        variant="secondary"
+                                                        size="md"
+                                                        onClick={deleteWatchlist}
+                                                        aria-pressed={active}
+                                                        className="btn btn-secondary"
+                                                    >
+                                                        Unadd
+                                                    </button>
+                                                    :
+                                                    <button
+                                                        id={movie.id}
+                                                        variant="outline-secondary"
+                                                        size="md"
+                                                        onClick={addWatchlist}
+                                                        aria-pressed={active}
+                                                        className="btn btn-outline-secondary"
+                                                    >
+                                                        Watchlist
+                                                    </button>
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
+
+
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
-    </>
-  );
+            <section className="section p-3 bg-light border-bottom border-2 border-dark">
+                <div className="container d-flex justify-content-between align-items-center">
+                    <div></div>
+
+                    <div className="justify-content-md-end">
+                        {renderPagination()}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
-export default Latest;
