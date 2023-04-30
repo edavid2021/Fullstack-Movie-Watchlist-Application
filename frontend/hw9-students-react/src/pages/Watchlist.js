e6907b0c3c9f9280d812b0a0602a2edb8cbdc347
giacobn <giacobn@comcast.net>
2023-04-30 19:21:29 -0400

    Watchlist working

--- frontend/hw9-students-react/src/pages/Watchlist.js
+++ frontend/hw9-students-react/src/pages/Watchlist.js
@@ -1,114 +1,116 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from 'react-router-dom';
+
 function Watchlist() {
+  const [user] = useAuthState(auth);
+  const [userId, setUserId] = useState(null);
+  const [watchlist, setWatchlist] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      setUserId(user.uid);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    async function fetchWatchlist() {
+      if (userId) {
+        const response = await axios.get(`http://localhost:5678/users/${userId}/movie_list`);
+        const watchlist = response.data;
+        setWatchlist(watchlist);
+      }
+    }
+    fetchWatchlist();
+  }, [userId]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const movies = await Promise.all(
+        watchlist.map(async item => {
+          const response = await axios.get(`https://api.themoviedb.org/3/movie/${item.movie_id}`, {
+            params: {
+              api_key: '8634de54298be041f009b7c918664433',
+              language: 'en-US'
+            }
+          });
+          return response.data;
+        })
+      );
+      setMovies(movies);
+    }
+    fetchMovies();
+  }, [watchlist]);
+
+  function deleteWatchlist(id) {
+    fetch(`http://localhost:5678/users/${userId}/movies/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      alert(data.message);
+      setMovies(movies.filter(movie => movie.id !== id)); // update movies state
+    })
+    .catch(error => console.error(error));
+  }
   
 
   return (
     <>
-    <section className="section p-3 bg-light border-bottom border-2 border-dark" >
-                <div className="container d-flex justify-content-between align-items-center">
-                    <h3 className="text-center">Watchlist</h3>
-                
-                <div className="justify-content-md-end">
-                    <button className="btn btn-outline-dark me-2">1</button>
-                    <button className="btn btn-outline-dark me-2">2</button>
-                    <button className="btn btn-outline-dark">3</button>
-                </div>
-                </div> 
-            </section>
-            <section className="section p-3 bg-light">
-                <div class="container">
-                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
+    <section className="section p-3 bg-light border-bottom border-2 border-dark">
+      <div className="container d-flex justify-content-between align-items-center">
+        <h3 className="text-center">Your Watchlist:</h3>
+      </div>
+    </section>
+    <section className="section p-3 bg-light">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+          {movies.map((movie) => {
+            return (
+              <div className="col d-flex" key={movie.id}>
+                <Link to={`/MovieDetails/${movie.id}`} className="movie-link">
+                  <div className="card h-100">
+                    <img
+                      className="card-img-top"
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">{movie.title}</h5>
+                      <p className="card-text">{movie.overview.substring(0, 100)}...</p>
+                      <div className="d-flex justify-content-between align-items-center mt-auto">
+                        <p className="card-text">
+                          <small className="text-muted">
+                            {movie.release_date}
+                          </small>
+                        </p>
+                        <button
+                          id={movie.id}
+                          variant="secondary"
+                          size="md"
+                          onClick={(event) => { event.preventDefault(); deleteWatchlist(movie.id) }}
+                          className="btn btn-secondary"
+                        >
+                          Unadd
+                        </button>
+                      </div>
                     </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                        </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 pt-4">
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                        </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card h-100">
-                        <img class="card-img-top" src="..." alt="Card image cap"/>
-                        <div class="card-body">
-                            <h5 class="card-title">Movie title</h5>
-                            <p class="card-text">Movie Description</p>
-                            <p class="card-text"><small class="text-muted">Release Date</small></p>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </section>
-    </>
-  );
-}
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  </>
+  );}  
 
 export default Watchlist;
