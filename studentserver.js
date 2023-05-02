6cb0e6aa708176ddc85fcfff3ef4988396c9649d
jackkanth <109559844+jackkanth@users.noreply.github.com>
2023-05-02 13:14:32 -0400

    heroku fixing

--- studentserver.js
+++ studentserver.js
@@ -404,3 +404,10 @@ app.get('/trending', async (req, res) => {
   console.log(`Server started on port ${port}`);
   });
 
+  if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+}
