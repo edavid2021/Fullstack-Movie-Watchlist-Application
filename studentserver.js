f4ea97dc04fa58704d05cd857f23f5a49da727d2
jackkanth <109559844+jackkanth@users.noreply.github.com>
2023-05-02 13:32:34 -0400

    fixed file path

--- studentserver.js
+++ studentserver.js
@@ -407,9 +407,9 @@ app.get('/trending', async (req, res) => {
   });
 
   if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
     });
 }
