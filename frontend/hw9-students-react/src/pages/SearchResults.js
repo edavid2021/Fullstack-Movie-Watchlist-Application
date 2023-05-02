3837c3953dc9554ae4e8c52e87b79eafb03d8d83
giacobn <giacobn@comcast.net>
2023-05-01 20:53:07 -0400

    seaec working

--- frontend/hw9-students-react/src/pages/SearchResults.js
+++ frontend/hw9-students-react/src/pages/SearchResults.js
@@ -2,25 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
-
-
-
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function SearchResults() {
   const location = useLocation();
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
   const [active, setActive] = useState(false);
-  var [Watchlist, setWatchlist] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [user] = useAuthState(auth);
+  const [userId, setUserId] = useState(null);
   const url = window.location.href;
   const result = url.slice(42).replace(/%20/g, " ");
 
-  //replace all the '%20' in the url result with space
-  // const result = url.slice(42).replace(/%20/g, " ");
-
-
+  useEffect(() => {
+    if (user) {
+      setUserId(user.uid);
+    }
+  }, [user]);
+  useEffect(() => {
+    const fetchWatchlist = async () => {
+      if (userId) { // only fetch if userId is not null
+        const response = await axios.get(`http://localhost:5678/users/${userId}/movie_list`);
+        setWatchlist(response.data);
+        console.log(response.data);
+      }
+    };
+    fetchWatchlist();
+  }, [userId]);
   function fetchMovies() {
     var send = {
       query: result
@@ -30,7 +40,6 @@ export default function SearchResults() {
     axios.post(backend, send).then((res) => {
       const { data } = res;
       setMovies(data.results);
-      setTotalPages(data.total_pages);
     }).catch((err) => {
       alert(err);
     });
@@ -43,105 +52,50 @@ export default function SearchResults() {
     setSearchQuery(query);
   }, [location.search]);
 
-  function handlePageChange(page) {
-    setCurrentPage(page);
-  }
-
-  function renderPagination() {
-    const PAGE_RANGE_DISPLAYED = 5;
-    const MARGIN_PAGES_DISPLAYED = 1;
-    let pagination = [];
-
-    if (totalPages <= PAGE_RANGE_DISPLAYED) {
-      for (let i = 1; i <= totalPages; i++) {
-        pagination.push(
-          <button
-            key={i}
-            className={`btn btn-outline-dark me-2 ${i === currentPage ? 'active' : ''}`}
-            onClick={() => handlePageChange(i)}
-          >
-            {i}
-          </button>
-        );
-      }
-    } else {
-      let leftSide = currentPage - Math.floor(PAGE_RANGE_DISPLAYED / 2);
-      let rightSide = currentPage + Math.floor(PAGE_RANGE_DISPLAYED / 2);
-
-      if (leftSide < MARGIN_PAGES_DISPLAYED + 1) {
-        leftSide = MARGIN_PAGES_DISPLAYED + 1;
-        rightSide = PAGE_RANGE_DISPLAYED;
-      }
-
-      if (rightSide > totalPages - MARGIN_PAGES_DISPLAYED) {
-        leftSide = totalPages - PAGE_RANGE_DISPLAYED + 1;
-        rightSide = totalPages - MARGIN_PAGES_DISPLAYED;
-      }
-
-      pagination.push(
-        <button
-          key={1}
-          className={`btn btn-outline-dark me-2 ${1 === currentPage ? 'active' : ''}`}
-          onClick={() => handlePageChange(1)}
-        >
-          {1}
-        </button>
-      );
-
-      if (leftSide > MARGIN_PAGES_DISPLAYED + 1) {
-        pagination.push(
-          <span key="leftEllipsis" className="me-2">
-            ...
-          </span>
-        );
-      }
-
-      for (let i = leftSide; i <= rightSide; i++) {
-        pagination.push(
-          <button
-            key={i}
-            className={`btn btn-outline-dark me-2 ${i === currentPage ? 'active' : ''}`}
-            onClick={() => handlePageChange(i)}
-          >
-            {i}
-          </button>
-        );
-      }
-
-      if (rightSide < totalPages - MARGIN_PAGES_DISPLAYED) {
-        pagination.push(
-          <span key="rightEllipsis" className="me-2">
-            ...
-          </span>
-        );
-      }
-
-      pagination.push(
-        <button
-          key={totalPages}
-          className={`btn btn-outline-dark me-2 ${totalPages === currentPage ? 'active' : ''}`}
-          onClick={() => handlePageChange(totalPages)}
-        >
-          {totalPages}
-        </button>
-      );
-    }
-
-    return <div>{pagination}</div>;
-  }
-
   function addWatchlist(event) {
     event.preventDefault();
     var id = parseInt(event.target.id);
-    setWatchlist([...Watchlist, id]);
+    const payload = {
+      movie_id: id,
+      rating: null,
+      comments: null,
+      watched: false
+    };
+    fetch(`http://localhost:5678/users/${userId}/movies`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setWatchlist([...watchlist, { movie_id: id }]);
+      console.log(watchlist);
+      alert(data.message);
+    })
+    .catch(error => console.error(error));
     
+    // Disable button after click
+    event.target.disabled = true;
   }
-
-  function deleteWatchlist(event) {
-    event.preventDefault();
-    var id = parseInt(event.target.id);
-    setWatchlist(Watchlist.filter(item => item !== id));
-    
+  
+  
+  function deleteWatchlist(id) {
+    fetch(`http://localhost:5678/users/${userId}/movies/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setWatchlist(watchlist.filter(item => item.movie_id !== id));
+      alert(data.message);
+    })
+    .catch(error => console.error(error));
   }
 
   return (
@@ -149,10 +103,8 @@ export default function SearchResults() {
       <section className="section p-3 bg-light border-bottom border-2 border-dark">
         <div className="container d-flex justify-content-between align-items-center">
           <h3 className="text-center">Search Results</h3>
-
-          <div className="justify-content-md-end">
-            {renderPagination()}
-          </div>
+  
+         
         </div>
       </section>
       <section className="section p-3 bg-light">
@@ -170,36 +122,37 @@ export default function SearchResults() {
                     <div className="card-body d-flex flex-column">
                       <h5 className="card-title">{movie.title}</h5>
                       <p className="card-text">{movie.overview.substring(0, 100)}...</p>
-
+  
                       <div class="d-flex justify-content-between align-items-center mt-auto">
                         <p class="card-text">
                           <small class="text-muted">
                             {movie.release_date}
                           </small>
                         </p>
-                        {Watchlist.includes(movie.id) ?
-                          <button
-                            id={movie.id}
-                            variant="secondary"
-                            size="md"
-                            onClick={deleteWatchlist}
-                            aria-pressed={active}
-                            className="btn btn-secondary"
-                          >
-                            Unadd
-                          </button>
-                          :
-                          <button
-                            id={movie.id}
-                            variant="outline-secondary"
-                            size="md"
-                            onClick={addWatchlist}
-                            aria-pressed={active}
-                            className="btn btn-outline-secondary"
-                          >
-                            Watchlist
-                          </button>
-                        }
+                        {watchlist.some(item => item.movie_id === movie.id) ?
+                            <button
+                              id={movie.id}
+                              variant="secondary"
+                              size="md"
+                              onClick={() => deleteWatchlist(movie.id)}
+                              aria-pressed={active}
+                              className="btn btn-secondary"
+                            >
+                              Unadd
+                            </button>
+                            :
+                            <button
+                              id={movie.id}
+                              variant="outline-secondary"
+                              size="md"
+                              onClick={addWatchlist}
+                              aria-pressed={active}
+                              className="btn btn-outline-secondary"
+                            >
+                              Watchlist
+                            </button>
+                          }
+
                       </div>
                     </div>
                   </div>
@@ -212,12 +165,9 @@ export default function SearchResults() {
       <section className="section p-3 bg-light border-bottom border-2 border-dark">
         <div className="container d-flex justify-content-between align-items-center">
           <div></div>
-
-          <div className="justify-content-md-end">
-            {renderPagination()}
-          </div>
         </div>
       </section>
     </>
   );
+  
 }
